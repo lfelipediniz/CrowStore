7d092bcca495d81568a28d65a05861ba84dea941
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-06-04 19:18:51 -0300

    search fix

--- CrowStore - NextJS/components/WrapSearch/WrapSearch.js
+++ CrowStore - NextJS/components/WrapSearch/WrapSearch.js
@@ -2,65 +2,67 @@ import React, { useState, useEffect } from "react";
 import Footer from "../Footer";
 import Sidebar from "../Sidebar";
 import Navbar from "../Navbar";
-
-// import FilterTags from "../FilterTags";
-// import DisplayOptions from "../DisplayOptions"
 import ProductData from "../../fakedata/showcaseContent/products.json";
 
 const WrapSearch = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filters, setFilters] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [filters, setFilters] = useState([]);
-    const [filteredItems, setFilteredItems] = useState([]);
+  useEffect(() => {
+    // Update filtered items whenever searchTerm or filters change
+    const newFilteredItems = filterItems(ProductData, searchTerm, filters);
+    setFilteredItems(newFilteredItems);
+  }, [searchTerm, filters]);
 
-    useEffect(() => {
-        const newFilteredItems = filterItems(ProductData, searchTerm, filters);
-        setFilteredItems(newFilteredItems);
-    }, [searchTerm, filters]);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    };
+  const modifyFilters = (newFilters) => {
+    setFilters(newFilters);
+  };
 
-    const modifyFilters = (newFilters) => {
-        setFilters(newFilters);
-    }
+  const modifySearchTerm = (newTerm) => {
+    setSearchTerm(newTerm);
+  };
 
+  // Function to filter items based on searchTerm and filters
+  const filterItems = (items, searchTerm, filters) => {
+    // Convert items object to an array of values
+    const itemArray = Object.values(items);
 
-    const modifySearchTerm = (newTerm) => {
-        setSearchTerm(newTerm);
-    }
+    return itemArray.filter((item) => {
+      // Check if the item name includes the search term
+      const hasSearchTerm =
+        item.productName &&
+        item.productName.toLowerCase().includes(searchTerm.toLowerCase());
 
-    // Function to filter items based on searchTerm and filters
-    const filterItems = (items, searchTerm, filters) => {
-        items.filter((item) => {
-            // Check if the item name includes the search term
-            const hasSearchTerm = item.productName
-                .toLowerCase()
-                .includes(searchTerm.toLowerCase());
+      // Check if the item matches all filters
+      const hasAllFilters = filters.every((filter) => {
+        return (
+          item.productName &&
+          item.productName.toLowerCase().includes(filter.toLowerCase())
+        );
+      });
 
-            // Check if the item matches all filters
-            const hasAllFilters = filters.every((filter) => {
-                return item.productName.toLowerCase().includes(filter.toLowerCase());
-            });
+      return hasSearchTerm && hasAllFilters;
+    });
+  };
 
-            return hasSearchTerm && hasAllFilters;
-        });
-    };
+  return (
+    <>
+      {/* Render the Sidebar component */}
+      <Sidebar isOpen={isOpen} toggle={toggle} home />
 
-    return (
-        <>
-            <Sidebar isOpen={isOpen} toggle={toggle} home />
-            <Navbar toggle={toggle} home />
+      {/* Render the Navbar component */}
+      <Navbar toggle={toggle} home />
 
-            {/* <SearchBar onChange={modifySearchTerm} /> */}
-            {/* <FilterTags onChange={modifyFilters} /> */}
-            {/* {searchTerm && <Subtitle>{filteredItems.length} resultados para "{searchTerm}"</Subtitle>} */}
-            {/* <DisplayOptions items={filteredItems} /> */}
-            <Footer />
-        </>
-    );
+      {/* Your JSX content here */}
+      <Footer />
+    </>
+  );
 };
 
 export default WrapSearch;

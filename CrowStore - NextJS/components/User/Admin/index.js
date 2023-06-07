4f782daa3fa554fbd212a2bb4e42a65b9d128845
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-06-07 20:23:05 -0300

    addition of remove function

--- CrowStore - NextJS/components/User/Admin/index.js
+++ CrowStore - NextJS/components/User/Admin/index.js
@@ -1,28 +1,53 @@
 import React, { useState } from "react";
 import { WrapContent } from "../../ReusedComponents/WrapContent";
-import {
-  UserContainer,
-  SideNavContainer,
-  Button,
-  EditModeContainer,
-  EditModeButton,
-  EditModeOptions,
-  EditModeOption,
-} from "../UserElements";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { UserContainer, RemoveButton } from "../UserElements";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
 function Admin() {
+  const [editingMode, setEditingMode] = useState(false);
+  const [showAddCategory, setShowAddCategory] = useState(false);
+  const [categories, setCategories] = useState(["Documentation", "Calendar"]);
+
+  const toggleEditingMode = () => {
+    setEditingMode(!editingMode);
+  };
+
+  const toggleAddCategory = () => {
+    setShowAddCategory(!showAddCategory);
+  };
+
+  const handleAddCategory = () => {
+    const newCategory = prompt("Digite o nome da nova categoria:");
+    if (newCategory) {
+      setCategories([...categories, newCategory]);
+    }
+  };
+
+  const handleRemoveCategory = (index) => {
+    const updatedCategories = [...categories];
+    updatedCategories.splice(index, 1);
+    setCategories(updatedCategories);
+  };
+
   return (
     <WrapContent>
       <UserContainer>
         <Sidebar>
           <Menu>
-            <SubMenu label="Charts">
-              <MenuItem> Pie charts </MenuItem>
-              <MenuItem> Line charts </MenuItem>
-            </SubMenu>
-            <MenuItem> Documentation </MenuItem>
-            <MenuItem> Calendar </MenuItem>
+            {categories.map((category, index) => (
+              <MenuItem key={index}>
+                {category}
+                {editingMode && (
+                  <RemoveButton onClick={() => handleRemoveCategory(index)}>
+                    X
+                  </RemoveButton>
+                )}
+              </MenuItem>
+            ))}
+            {editingMode && (
+              <MenuItem onClick={handleAddCategory}> Adicionar Categoria </MenuItem>
+            )}
+            <MenuItem onClick={toggleEditingMode}> Modo Edição </MenuItem>
           </Menu>
         </Sidebar>
       </UserContainer>
--- CrowStore - NextJS/components/User/UserElements.js
+++ CrowStore - NextJS/components/User/UserElements.js
@@ -11,58 +11,9 @@ export const UserContainer = styled.div`
   background-color: ${colors.primary};
 `;
 
-export const SideNavContainer = styled.div`
-  background-color: #262626;
-  width: 276px;
-  height: 100%;
-`;
-
-export const SideNavItem = styled.div`
-  padding: 10px;
-  color: ${colors.primary};
-  cursor: pointer;
-`;
-
-export const Button = styled.button`
-  padding: 10px 20px;
-  background-color: ${colors.primary};
-  color: ${colors.white};
+export const RemoveButton = styled.button`
+  background: transparent;
   border: none;
-  border-radius: 4px;
-  font-size: 16px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${colors.cta};
-  }
-`;
-
-export const EditModeContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 1rem;
-`;
-
-export const EditModeButton = styled.button`
-  background-color: ${colors.primary};
-  color: ${colors.white};
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 4px;
-  margin-right: 1rem;
-  cursor: pointer;
-`;
-
-export const EditModeOptions = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
-export const EditModeOption = styled.button`
-  background-color: ${colors.primary};
-  color: ${colors.white};
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 4px;
+  color: red;
   cursor: pointer;
 `;

4942519769338a8bf1267186a8a9cdf0fd3e2606
Abreu <abreu@disroot.org>
2023-06-11 14:15:08 -0300

    Added json descriptor.

--- CrowStore - NextJS/components/WrapProduct/WrapProduct.jsx
+++ CrowStore - NextJS/components/WrapProduct/WrapProduct.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; (142)
+
 import Footer from "../Footer";
 import Sidebar from "../Sidebar";
 import Navbar from "../Navbar";
 import ShowCaseSection from "../ShowCaseSection";
+import { BodyContainer } from "../WrapShopCart/WrapElements";
 
-const WrapProduct = () => {
+const WrapProduct = ({ product }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => {
@@ -15,9 +17,24 @@ const WrapProduct = () => {
         <>
             <Sidebar isOpen={isOpen} toogle={toggle} home />
             <Navbar toogle={toggle} home />
-            <ShowCaseSection subtitle="Esta peça combina com"></ShowCaseSection>
+            <BodyContainer>
+                <Visualizer product={product} />
+                <Description product={product} />
+                <PurchaceOptions product={product} />
+                <ScrollableContainer style={{ marginLeft: 0 }}>
+                    <ProductContainer>
+                        {filteredItems.map((product, index) => (
+                            <ProductCard
+                                key={index}
+                                img={product.image}
+                                productName={product.productName}
+                                price={product.price}
+                            />
+                        ))}
+                    </ProductContainer>
+                </ScrollableContainer>
+            </BodyContainer>
             <Footer />
-            {/* <DisplayProduct /> */}
         </>
     );
 }

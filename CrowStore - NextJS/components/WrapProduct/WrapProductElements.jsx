946d427bfc6229da819b54ff95b054af1eb1efc8
Abreu <abreu@disroot.org>
2023-06-11 18:20:59 -0300

    Error

---
+++ CrowStore - NextJS/components/WrapProduct/WrapProductElements.jsx
@@ -0,0 +1,16 @@
+import { colors } from "../../styles/colors";
+import styled from "styled-components";
+
+export const BodyContainer = styled.div`
+    max-width: 1184px;
+    margin: 120px auto;
+    display:flex;
+    flex-wrap:wrap;
+
+    * {    
+        font-family: Inter, sans-serif;
+        font-size: 1em;
+        line-height: 1.5em;
+        box-sizing: border-box;
+    }
+`

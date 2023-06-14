004e330c41dd8d168727fc9c16aca579559c8485
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-06-14 09:54:27 -0300

    vercel

--- components/DisplayOptions/index.jsx
+++ components/DisplayOptions/index.jsx
@@ -1,54 +1,54 @@
 import React, { useState, useEffect } from 'react';
 
 const DisplayOptions = ({ items }) => {
-  const [itemsPerPage, setItemsPerPage] = useState(5);
+    const [itemsPerPage, setItemsPerPage] = useState(5);
 
-  const updateItemsPerPage = () => {
-    const screenWidth = window.innerWidth;
+    const updateItemsPerPage = () => {
+        const screenWidth = window.innerWidth;
+        let newItemsPerPage = 5;
 
-    if (screenWidth < 570) {
-      setItemsPerPage(1);
-    } else {
-      const additionalItems = Math.floor((screenWidth - 570) / 238);
-      const newItemsPerPage = Math.max(1, additionalItems + 1);
-      setItemsPerPage(newItemsPerPage);
-    }
-  };
+        if (screenWidth < 570) {
+            newItemsPerPage = 1;
+        } else if (screenWidth < 808) {
+            newItemsPerPage = 2;
+        } else if (screenWidth < 1050) {
+            newItemsPerPage = 3;
+        } else if (screenWidth < 1340) {
+            newItemsPerPage = 4;
+        }
 
-  useEffect(() => {
-    updateItemsPerPage();
-    window.addEventListener('resize', updateItemsPerPage);
-    return () => window.removeEventListener('resize', updateItemsPerPage);
-  }, []);
+        setItemsPerPage(newItemsPerPage);
+    };
 
-  const productRows = [];
-  for (let i = 0; i < items.length; i += itemsPerPage) {
-    const row = [];
-    for (let j = i; j < i + itemsPerPage; j++) {
-      if (j < items.length) {
-        const product = items[j];
-        row.push(
-          <a href="" key={j}>
-            <div
-              className="suggestion"
-              style={{ backgroundImage: `url(../../../${product.image})` }}
-            ></div>
-            <p className="name">{product.productName}</p>
-            <p className="price">{product.price}</p>
-          </a>
+    useEffect(() => {
+        updateItemsPerPage();
+        window.addEventListener('resize', updateItemsPerPage);
+        return () => window.removeEventListener('resize', updateItemsPerPage);
+    }, []);
+
+    const productRows = [];
+    for (let i = 0; i < items.length; i += itemsPerPage) {
+        const row = [];
+        for (let j = i; j < i + itemsPerPage; j++) {
+            if (j < items.length) {
+                const product = items[j];
+                row.push(
+                    <a href="" key={j}>
+                        <div className="suggestion" style={{ backgroundImage: `url(../../../${product.image})` }}></div>
+                        <p className="name">{product.productName}</p>
+                        <p className="price">{product.price}</p>
+                    </a>
+                );
+            } else {
+                break;
+            }
+        }
+        productRows.push(
+            <div className="row" key={i} style={{ clear: 'both' }}> {row} </div>
         );
-      } else {
-        break;
-      }
     }
-    productRows.push(
-      <div className="row" key={i} style={{ clear: 'both' }}>
-        {row}
-      </div>
-    );
-  }
 
-  return <nav id="carousel">{productRows}</nav>;
+    return <nav id="carousel">{productRows}</nav>;
 };
 
 export default DisplayOptions;

b23746bb1c9f11252f7b1297e33c5c567aca3a6b
Abreu <abreu@disroot.org>
2023-06-24 14:33:24 -0300

    Added firebase to the project.

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,20 @@ import '../styles/globals.css'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+import firebase from './_firebase.jsx'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+    const router = useRouter()
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const loader = document.getElementById('globalLoader');
-      if (loader)
-        loader.remove();
-    }
-  }, []);
-  
-  return <Component {...pageProps} />
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const loader = document.getElementById('globalLoader');
+            if (loader)
+                loader.remove();
+        }
+    }, []);
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp

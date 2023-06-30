beceaa970e6d159096d8783234a18956932a418a
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-06-30 18:11:06 -0300

    register a user on the system

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -30,6 +31,36 @@ module.exports = class UserController {
     if (password !== confirmpassword) {
       res.status(422).json({ message: "As senhas precisam ser iguais!" });
       return;
+    }
+
+    // check if user exists
+
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: "Email já cadastrado!" });
+      return;
+    }
+
+    // create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create a user
+
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+        const newUser = await user.save()
+        res.status(201).json({ message: "Usuário criado!", newUser});
+
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   }
 };

730a235d1c596dabf2edc967868ec5af54d4b407
Abreu <abreu@disroot.org>
2023-07-03 10:39:14 -0300

    Working on image uploading

--- backend/helpers/image-upload.js
+++ backend/helpers/image-upload.js
@@ -2,3 +2,25 @@ const multer = require("multer")
 const path = require("path")
 
 // Destination to store the images
+const imageStore = multer.diskStorage({
+    destination: function(req, file, cb) {
+        let folder = req.baseUrl.includes("users") ? "users" : "products";
+
+        cb(null, `public/images/${folder}`)
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname))
+    },
+})
+
+const imageUpload = multer({
+    storage: imageStore,
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
+            return cb(new Error("Apenas .png ou .jpg são formatos de imagem válidos"))
+        }
+        cb(undefined, true)
+    },
+})
+
+module.exports = { imageUpload }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -5,13 +5,18 @@ const User = require('../models/User')
 
 // middleware
 const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload.js')
 
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
-
+router.patch(
+    '/edit/:id',
+    verifyToken,
+    imageUpload.single("image"),
+    UserController.editUser
+)
 
 module.exports = router

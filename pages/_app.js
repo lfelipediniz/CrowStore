5a48eae5a9d0983921d14c9f0bdc8d202bbeec8a
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-07-03 03:10:39 -0300

    creating user context

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import '../styles/globals.css'
-
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-
+import { UserProvider } from '../context/UserContext'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -15,7 +14,11 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
   
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp

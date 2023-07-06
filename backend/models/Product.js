42afbad90ba450b64917d1213fe1c5da77739213
Abreu <abreu@disroot.org>
2023-07-06 19:56:48 -0300

    Defined Purchases Schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,70 +1,43 @@
 const mongoose = require('../db/conn');
-const { Schema } = mongoose;
+const Model = require('./Model');
 
-const Model = new mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
-    productId: {
-        type: String,
-        required: true
-    },
-    size: {
-        type: String,
-        enum: ['PP', 'P', 'M', 'G', 'XG'],
-        required: true
-    },
-    color: {
-        type: String,
-        enum: ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'],
-        required: true
-    },
-    quantity: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-                return Number.isInteger(value) && value >= 0;
-            },
-            message: 'A quantidade necessita ser um valor positivo e inteiro.'
+const Product = mongoose.model(
+    'Product',
+    new mongoose.Schema({
+        _id: mongoose.Types.ObjectId,
+        name: {
+            type: String,
+            unique: true,
+            required: true
         },
-        required: true
-    }
-}, { timestamps: true });
-
-const Product = new Schema({
-    _id: mongoose.Types.ObjectId,
-    name: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    tags: {
-        type: Array,
-        required: true
-    },
-    gender: {
-        type: String,
-        enum: ['Masculino', 'Feminino', 'Unissex'],
-        required: true
-    },
-    price: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-                return value >= 0;
-            },
-            message: 'O preço necessita ser um valor positivo'
+        tags: {
+            type: Array,
+            required: true
         },
-        required: true
-    },
-    images: {
-        type: Array,
-        required: true
-    },
-    AvailableModels: {
-        type: [Model],
-        sparse: true
-    }
-}, { timestamps: true });
+        gender: {
+            type: String,
+            enum: ['Masculino', 'Feminino', 'Unissex'],
+            required: true
+        },
+        price: {
+            type: Number,
+            validate: {
+                validator: function(value) {
+                    return value >= 0;
+                },
+                message: 'O preço necessita ser um valor positivo'
+            },
+            required: true
+        },
+        images: {
+            type: Array,
+            required: true
+        },
+        AvailableModels: {
+            type: [Model],
+            sparse: true
+        }
+    }, { timestamps: true })
+);
 
-const ProductModel = mongoose.model('Product', Product);
-
-module.exports = ProductModel;
+module.exports = mongoose.model('Product', Product);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,74 +1,70 @@
 const mongoose = require("../db/conn");
 const { Schema } = mongoose;
+const Model = require('./Model');
 
-const Purchase = new mongoose.Schema({
-  quantity: {
-    type: Number,
-    validate: {
-      validator: function (value) {
-        return Number.isInteger(value) && value >= 0;
-      },
-      message: "A quantidade necessita ser um valor positivo e inteiro.",
+const Purchase = new Schema({
+    _id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        unique: true
     },
-    required: true,
-  },
-  productId: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  size: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true
-  },
-  qtd: {
-    type: Number,
-    required: true
-  }
+    productId: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    },
+    model: {
+        type: Model,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        validate: {
+            validator: function(value) {
+                return Number.isInteger(value) && value >= 1 && value <= this.model.quantity;
+            },
+            message: 'A quantidade deve ser um número inteiro maior ou igual a 1 e menor ou igual à quantidade disponível para esse modelo.'
+        },
+        required: true
+    }
 });
 
+module.exports = Purchase;
+
 const User = mongoose.model(
-  "User",
-  new Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String,
-      },
-      phone: {
-        type: String,
-        required: true,
-      },
-      cpf: {
-        type: String,
-        required: true,
-      },
-      admin: {
-        type: Boolean,
-      },
-      cart: [Purchase],
-      shopping: [],
-    },
-    { timestamps: true }
-  )
+    "User",
+    new Schema(
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+            },
+            password: {
+                type: String,
+                required: true,
+            },
+            image: {
+                type: String,
+            },
+            phone: {
+                type: String,
+                required: true,
+            },
+            cpf: {
+                type: String,
+                required: true,
+            },
+            admin: {
+                type: Boolean,
+            },
+            cart: [Purchase],
+            shopping: [],
+        },
+        { timestamps: true }
+    )
 );
 
 module.exports = User;

4a52442fa9ddeb9aaa05bcb9274a0c5886fec728
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-07-09 12:01:12 -0300

    bug fix

--- components/ProductDescription/index.jsx
+++ components/ProductDescription/index.jsx
@@ -2,67 +2,72 @@ import React, { useState } from "react";
 import ProductGallery from "../ProductGallery";
 import ProductInfo from "../ProductInfo";
 import jwt from "jsonwebtoken";
-import { ProductContainer, ProductImageContainer } from "./ProductDescriptionElements";
+import {
+  ProductContainer,
+  ProductImageContainer,
+} from "./ProductDescriptionElements";
 
 const ProductDescription = ({ product }) => {
-    const [selectedImage, setSelectedImage] = useState(`/CrowStore/imgs/${product.images[0]}`);
+  const [selectedImage, setSelectedImage] = useState(
+    `/CrowStore/imgs/${product.images[0]}`
+  );
 
-    const handleImageSelect = (image) => {
-        setSelectedImage(image);
+  const handleImageSelect = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleAddToCart = (item) => {
+    const newItem = {
+      name: item.product,
+      price: item.price,
+      color: item.color,
+      size: item.size,
+      quantity: 1,
     };
 
-    const handleAddToCart = () => {
-        const newItem = {
-            name: product.name,
-            price: product.price,
-            color: product.color,
-            size: product.size,
-            quantity: 1
-        };
+    // Retrieve the token from local storage
+    const token = localStorage.getItem("token");
 
-        // Retrieve the token from local storage
-        const token = localStorage.getItem("token");
+    // Decode the token to extract the user ID
+    const decodedToken = jwt.decode(token);
+    const userId = decodedToken.id;
 
-        // Decode the token to extract the user ID
-        const decodedToken = jwt.decode(token);
-        const userId = decodedToken.id;
+    // Send the newItem to the user's cart
+    fetch(`http://localhost:5000/users/${userId}/cart`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to add the product to the cart.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the response from the server
+        if (data.message) {
+          alert(data.message);
+        } else {
+          alert("Failed to add the product to the cart.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred while adding the product to the cart.");
+      });
+  };
 
-        // Send the newItem to the user's cart
-        fetch(`http://localhost:5000/users/${userId}/cart`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}` // Include the token in the Authorization header
-            },
-            body: JSON.stringify(newItem)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Failed to add the product to the cart.");
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Handle the response from the server
-                if (data.message) {
-                    alert(data.message);
-                } else {
-                    alert('Failed to add the product to the cart.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('An error occurred while adding the product to the cart.');
-            });
-    };
-
-    return (
-        <ProductContainer>
-            <ProductGallery images={product.images} onSelect={handleImageSelect} />
-            <ProductImageContainer src={selectedImage} />
-            <ProductInfo product={product} onAddToCart={handleAddToCart} />
-        </ProductContainer>
-    );
-}
+  return (
+    <ProductContainer>
+      <ProductGallery images={product.images} onSelect={handleImageSelect} />
+      <ProductImageContainer src={selectedImage} />
+      <ProductInfo product={product} onAddToCart={handleAddToCart} />
+    </ProductContainer>
+  );
+};
 
 export default ProductDescription;

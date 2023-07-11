82a047f46973afd62f78e213b66f4bec66627b5f
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-07-11 02:46:23 -0300

    bug fix

--- components/User/common.js
+++ components/User/common.js
@@ -1,21 +1,82 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { Button } from "@mui/material";
-import Modal from "@mui/material/Modal";
 import { MdPerson, MdShoppingCart } from "react-icons/md";
 import jwt from "jsonwebtoken";
 import axios from "axios";
 import Cart from "../Cart";
 import { colors } from "../../styles/colors";
+import { List, ListItem, ListItemText, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Avatar from "@mui/material/Avatar";
+import Modal from "@mui/material/Modal";
+
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 function CommonUser() {
   const [userData, setUserData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("perfil");
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [senha, setPassword] = useState("");
+  const [confirmSenha, setConfirmSenha] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [isPhoneInvalid, setIsPhoneInvalid] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [isPhoneFocused, setIsPhoneFocused] = useState(false);
+
+  const [userN, setUsername] = useState("");
+  const [isCadastrando, setIsCadastrando] = useState(false);
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
+  const [isCPFFocused, setIsCPFFocused] = useState(false);
+  const [isCPFInvalid, setIsCPFInvalid] = useState(false);
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  const [isConfirmSenhaFocused, setIsConfirmSenhaFocused] = useState(false);
+
   const [cartData, setCartData] = useState({
     Products: [],
     Quantities: [],
   });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const updatedUser = {
+        name: nome,
+        phone: telefone,
+        password: senha,
+      };
+
+      const token = localStorage.getItem("token");
+      const decodedToken = jwt.decode(token);
+
+      if (decodedToken && decodedToken.id) {
+        const userId = decodedToken.id;
+
+        const response = await axios.patch(
+          `http://localhost:5000/users/edit/${userId}`,
+          updatedUser,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          alert("Usuário atualizado com sucesso");
+          // Atualizar os dados do usuário exibidos no perfil
+          fetchUserData(userId);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao atualizar usuário:", error);
+      // Exibir uma mensagem de erro ao usuário, caso necessário
+    }
+  };
 
   const fetchUserData = async (userId) => {
     try {
@@ -82,6 +143,169 @@ function CommonUser() {
     });
   };
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleAvatarChange = (event) => {
+    setAvatar(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleCpfChange = (event) => {
+    setCpf(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleCPFChange = (event) => {
+    const rawValue = event.target.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    const formattedValue = formatCPF(rawValue); // Formata o CPF
+
+    // Verifica se o CPF possui 11 dígitos
+    if (formattedValue.length < 14) {
+      setIsCPFInvalid(true);
+    } else {
+      setIsCPFInvalid(false);
+    }
+
+    setCpf(formattedValue);
+  };
+
+  const handleTelefoneChange = (event) => {
+    const rawValue = event.target.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    const formattedValue = formatPhoneNumber(rawValue); // Formata o número de telefone
+
+    // Verifica se o número de telefone possui pelo menos 11 dígitos
+    if (formattedValue.length >= 15) {
+      setIsPhoneInvalid(false);
+    } else {
+      setIsPhoneInvalid(true);
+    }
+
+    setTelefone(formattedValue);
+  };
+  const handleEmailChange = (event) => {
+    const emailValue = event.target.value;
+
+    // Verifica se o email é válido usando uma expressão regular
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(emailValue);
+
+    setIsEmailInvalid(!isValidEmail);
+    setEmail(emailValue);
+  };
+
+  const handleNomeChange = (event) => {
+    setNome(event.target.value);
+  };
+
+  const handleConfirmSenhaChange = (event) => {
+    setConfirmSenha(event.target.value);
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const handleFormlogin = (event) => {
+    // Lógica para fazer login
+  };
+
+  const handleFormsignup = (event) => {
+    if (
+      senha !== confirmSenha ||
+      isCPFInvalid ||
+      isEmailInvalid ||
+      isPhoneInvalid ||
+      !cpf ||
+      !senha ||
+      !confirmSenha ||
+      !email ||
+      !nome ||
+      !telefone
+    ) {
+      // Alguma informação está faltando ou inválida, não envie o formulário
+      return;
+    }
+
+    // Resto do código para cadastrar o usuário
+
+    // Exemplo de alerta
+    alert("Usuário cadastrado com sucesso");
+
+    // Reiniciar o estado do formulário
+    setCpf("");
+    setTelefone("");
+    setEmail("");
+    setNome("");
+    setConfirmSenha("");
+    setIsCadastrando(false);
+  };
+
+  const formatCPF = (value) => {
+    // Implemente a lógica de formatação do CPF aqui, por exemplo:
+    const cpfRegex = /^(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})$/;
+    const match = value.match(cpfRegex);
+
+    if (match) {
+      return `${match[1]}.${match[2]}.${match[3]}-${match[4]}`;
+    }
+
+    return value;
+  };
+
+  const formatPhoneNumber = (value) => {
+    const phoneRegex = /^(\d{0,2})(\d{0,5})(\d{0,4})$/;
+    const match = value.match(phoneRegex);
+
+    if (match) {
+      if (match[1] && match[2] && match[3]) {
+        return `(${match[1]}) ${match[2]}-${match[3]}`;
+      } else if (match[1] && match[2]) {
+        return `(${match[1]}) ${match[2]}`;
+      } else if (match[1]) {
+        return `(${match[1]}`;
+      }
+    }
+
+    return value;
+  };
+
+  const handleEmailFocus = (value) => {
+    console.log("ai calica");
+  };
+
+  const handlePhoneFocus = () => {
+    setIsPhoneFocused(true);
+  };
+
+  const handlePhoneBlur = () => {
+    setIsPhoneFocused(false);
+    if (telefone.length <= 11) {
+      setIsPhoneInvalid(true);
+    } else {
+      setIsPhoneInvalid(false);
+    }
+  };
+
   return (
     <div style={{ backgroundColor: colors.primary }}>
       <div className="user-info-container">
@@ -144,7 +368,109 @@ function CommonUser() {
       <Modal open={isModalOpen} onClose={handleModalClose}>
         <div className="modal-container">
           <h2>Editar Perfil</h2>
-          {/* Formulário de edição de perfil */}
+          <Modal open={isModalOpen} onClose={handleModalClose}>
+            <div className="modal-container">
+              <h2>Editar Perfil</h2>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Novo Nome"
+                  required
+                  fullWidth
+                  value={nome}
+                  onChange={handleNomeChange}
+                  variant="standard"
+                  size="small"
+                  InputProps={{
+                    autoComplete: "new-name",
+                  }}
+                />{" "}
+                <br /> <br />
+                <TextField
+                  label="Novo Telefone"
+                  required
+                  error={isPhoneInvalid}
+                  value={telefone}
+                  fullWidth
+                  onChange={handleTelefoneChange}
+                  onFocus={() => setIsPhoneFocused(true)}
+                  onBlur={() => setIsPhoneFocused(false)}
+                  variant="standard"
+                  size="small"
+                  InputProps={{
+                    autoComplete: "new-phone",
+                    inputProps: { maxLength: 15 }, // Define o máximo de caracteres como 11
+                  }}
+                />
+                <br /> <br />
+                <TextField
+                  label="Nova Senha"
+                  required
+                  type={showPassword ? "text" : "password"}
+                  value={senha}
+                  onChange={handlePasswordChange}
+                  variant="standard"
+                  size="small"
+                  fullWidth
+                  InputProps={{
+                    endAdornment: (
+                      <Button
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {!showPassword ? (
+                          <FaEyeSlash style={{ color: colors.primary }} />
+                        ) : (
+                          <FaEye style={{ color: colors.primary }} />
+                        )}
+                      </Button>
+                    ),
+                    autoComplete: "new-password",
+                  }}
+                />
+                <br />
+                <br />
+                <TextField
+                  label="Confirmar Nova Senha"
+                  required
+                  type={showConfirmPassword ? "text" : "password"}
+                  value={confirmSenha}
+                  onChange={handleConfirmSenhaChange}
+                  onFocus={() => setIsConfirmSenhaFocused(true)}
+                  onBlur={() => setIsConfirmSenhaFocused(false)}
+                  variant="standard"
+                  size="small"
+                  fullWidth
+                  InputProps={{
+                    endAdornment: (
+                      <Button onClick={handleClickShowConfirmPassword}>
+                        {!showConfirmPassword ? (
+                          <FaEyeSlash style={{ color: colors.primary }} />
+                        ) : (
+                          <FaEye style={{ color: colors.primary }} />
+                        )}
+                      </Button>
+                    ),
+                    autoComplete: "new-password",
+                  }}
+                />
+                <br />
+                <br />
+                <center>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    style={{
+                      backgroundColor: colors.cta,
+                      color: "white",
+                    }}
+                  >
+                    Atualizar Usuário
+                  </Button>
+                </center>
+                <br />
+              </form>
+            </div>
+          </Modal>
         </div>
       </Modal>
 

35e6cafc60cad0210b1a43bcdcb6ab870f218b09
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-07-12 19:23:34 -0300

    bug cart fix

--- components/WrapShopCart/index.jsx
+++ components/WrapShopCart/index.jsx
@@ -87,14 +87,19 @@ const WrapShopCart = () => {
   };
 
   const handleUpdateCartItem = async (modelIndex, newQuantity) => {
-    const productId = await getProductIdByName(cartData.Products[modelIndex].name);
+    const productId = await getProductIdByName(
+      cartData.Products[modelIndex].name
+    );
     const diff = newQuantity - cartData.Quantities[modelIndex];
-  
+
     if (productId) {
-      await axios.patch(`http://localhost:5000/products/updateProductModel/${productId}`, {
-        modelIndex,
-        newQuantity
-      });
+      await axios.patch(
+        `http://localhost:5000/products/updateProductModel/${productId}`,
+        {
+          modelIndex,
+          newQuantity,
+        }
+      );
 
       const updatedQuantities = [...cartData.Quantities];
       updatedQuantities[modelIndex] = newQuantity;
@@ -124,54 +129,50 @@ const WrapShopCart = () => {
       const token = localStorage.getItem("token");
       const decodedToken = jwt.decode(token);
       const userId = decodedToken.id;
-  
+
       console.log("Carrinho antes da atualização:", userData.cart); // Verificar o carrinho antes da atualização
-  
+
       for (let i = 0; i < userData.cart.length; i++) {
         const item = userData.cart[i];
         const modelIndex = item.modelIndex;
         const modelIndexx = item.product.modelIndex;
         const newQuantity = item.product.quantity;
-        
-        console.log("Atualizando item:", item); // Verificar o item atual antes da atualização
-  
-        const productId = await getProductIdByModelIndex(modelIndex); // Obter o ID do produto com base no índice do modelo
-        console.log(modelIndexx)
-        console.log(newQuantity)
-        if (productId) {
 
+        console.log("Atualizando item:", item); // Verificar o item atual antes da atualização
+
+        const productId = await getProductIdByModelIndex(modelIndex); // Obter o ID do produto com base no índice do modelo
+        console.log(modelIndexx);
+        console.log(newQuantity);
+        if (productId) {
           const Newproduto = {
-            productId:  productId,
+            productId: productId,
             modelIndex: modelIndexx,
             newQuantity: newQuantity,
           };
-          
-          return fetch("http://localhost:5000/products/updateProductModel/", {
+
+          await fetch("http://localhost:5000/products/updateProductModel/", {
             method: "PATCH",
             headers: {
-                "Content-Type": "application/json",
+              "Content-Type": "application/json",
             },
             body: JSON.stringify(Newproduto),
-        })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Erro ao atualizar produto");
+          }).then((response) => {
+            if (!response.ok) {
+              throw new Error("Erro ao atualizar produto");
+            }
+          });
         }
-      });
-  };
-   
-  }
+      }
 
-  
       await axios.patch(`http://localhost:5000/users/${userId}/cart/finalize`);
-  
+
       alert("Carrinho finalizado com sucesso!");
       window.location.reload();
     } catch (error) {
       console.error("Erro ao finalizar o carrinho:", error);
-      alert("Erro ao finalizar o carrinho. Por favor, tente novamente mais tarde.");
+      alert(
+        "Erro ao finalizar o carrinho. Por favor, tente novamente mais tarde."
+      );
     }
   };
 
@@ -180,12 +181,12 @@ const WrapShopCart = () => {
       const token = localStorage.getItem("token");
       const decodedToken = jwt.decode(token);
       const userId = decodedToken.id;
-  
+
       const userResponse = await axios.get(
         `http://localhost:5000/users/${userId}`
       );
       const userCart = userResponse.data.cart;
-  
+
       const item = userCart.find((item) => item.modelIndex === modelIndex);
       if (item) {
         const productName = item.product.name;

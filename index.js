3f690cc4b855025269d9bb24b6d2fc6107fc64be
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-03-27 17:12:15 -0300

    up

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json())
 //Solve CORS
 app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN || 'http://localhost:3000' }));
 
-
 // Public folder for images
 app.use(express.static('public'))
 
